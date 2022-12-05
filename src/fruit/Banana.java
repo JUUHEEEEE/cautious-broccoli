eebcefd1c2b7de7f8ac5fe03b49539b10d5ae55e
Gi7D-12 <Gi7D-12@DESKTOP-G4C1RKR>
2022-12-05 14:25:31 +0900

    바나나에 기능 추가

--- src/fruit/Banana.java
+++ src/fruit/Banana.java
@@ -4,4 +4,15 @@ public class Banana {
 
 	int price;
 	int grade;
+	
+	public Banana() {
+		this(100, 100);
+	}
+
+	public Banana(int price, int grade) {
+		this.price = price;
+		this.grade = grade;
+	}
+	
+	
 }
